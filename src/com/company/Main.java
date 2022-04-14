82327152c87d223c5a7d47067967b85759d17cd3
anhtay <tayphongtran@gmail.com>
2022-04-14 17:17:13 +0700

    up

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Fan fan1 = new Fan(3, true, 5,"blue");
+        Fan fan2 = new Fan(2, true, 10,"blue");
+        System.out.println( fan1 + " \n" + fan2);
+    }
+}
